3db93d838bc995a20724e19e43d592adbac2ce7b
Kajetan Tukendorf <34398883+Bacoknight@users.noreply.github.com>
2024-05-19 22:37:24 +0100

    Updated for end of hackathon

--- llm.py
+++ llm.py
@@ -10,31 +10,43 @@ from llama_index.core import VectorStoreIndex
 from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.vector_stores.chroma import ChromaVectorStore
 import tqdm
+from llama_index.core.vector_stores import ExactMatchFilter, MetadataFilters
+from llama_index.core.vector_stores import (
+    MetadataFilter,
+    MetadataFilters,
+    FilterOperator,
+)
 
 load_dotenv()
 
 # ----- DATA CLEANING ----- #
 
 # Download giftcards products and reviews
-giftcards_reviews_dataset = load_dataset("McAuley-Lab/Amazon-Reviews-2023", "raw_review_Gift_Cards", trust_remote_code=True)
-giftcards_meta_dataset = load_dataset("McAuley-Lab/Amazon-Reviews-2023", "raw_meta_Gift_Cards", trust_remote_code=True)
+giftcards_reviews_dataset = load_dataset("McAuley-Lab/Amazon-Reviews-2023", "raw_review_Amazon_Fashion", trust_remote_code=True)
+giftcards_meta_dataset = load_dataset("McAuley-Lab/Amazon-Reviews-2023", "raw_meta_Amazon_Fashion", trust_remote_code=True)
 embed_model = OpenAIEmbedding(model="text-embedding-3-small")
 
 # Convert to pandas df
-giftcards_reviews_df = pd.DataFrame(giftcards_reviews_dataset['full'][0:500])
-giftcards_meta_df = pd.DataFrame(giftcards_meta_dataset['full'][0:500])
+giftcards_reviews_df = pd.DataFrame(giftcards_reviews_dataset['full'])
+giftcards_meta_df = pd.DataFrame(giftcards_meta_dataset['full'])
 
 # For each product, add all relevant reviews.
-# Combine all reviews for a given parent ASIN.
+# Combine all reviews for a given parent ASIN, truncating text to fit chunk size.
 giftcards_reviews_df = giftcards_reviews_df.groupby('parent_asin').agg({'text': '\n'.join}).reset_index()
+giftcards_reviews_df.loc[:, 'text'] = giftcards_reviews_df['text'].apply(lambda x: x[:9000])
 giftcards_reviews_df = giftcards_reviews_df.merge(giftcards_meta_df, on='parent_asin')
 
 # Create a new df with only the relevant columns
 giftcards_df = giftcards_reviews_df[['parent_asin', 'text', 'title', 'average_rating', 'rating_number', 'features', 'description', 'price', 'images', 'details', 'subtitle']]
 
+# Remove rows with no price data and convert to float
+giftcards_df = giftcards_df.dropna(subset=["price", "images"])
+giftcards_df["price"] = giftcards_df["price"].apply(lambda x: x.replace("None", "0"))
+giftcards_df["price"] = pd.to_numeric(giftcards_df["price"])
+
 # Convert columns to types that the LLM can handle
 giftcards_df.loc[:, "images"] = giftcards_df["images"].apply(lambda x: x.get("hi_res", None) if isinstance(x, dict) else None)
-giftcards_df.loc[:, "images"] = giftcards_df["images"].apply(lambda x: x[0] if isinstance(x, list) else x)
+giftcards_df.loc[:, "images"] = giftcards_df["images"].apply(lambda x: x[0] if isinstance(x, list) and len(x) > 0 else None)
 giftcards_df.loc[:, "features"] = giftcards_df["features"].apply(lambda x: '\n'.join(x) if isinstance(x, list) else x)
 
 # ----- LLM PREP ----- #
@@ -65,16 +77,17 @@ vector_store = ChromaVectorStore(chroma_collection=giftcards_collection)
 
 # ----- ADD RECORDS ----- #
 
-# parser = SentenceSplitter(chunk_size=5000)
-# nodes = parser.get_nodes_from_documents(documents, show_progress=True)
+# UNCOMMENT TO BUILD VECTOR STORE
+parser = SentenceSplitter(chunk_size=10000)
+nodes = parser.get_nodes_from_documents(documents, show_progress=True)
 
-# for node in tqdm.tqdm(nodes):
-#     node_embedding = embed_model.get_text_embedding(
-#         node.get_content(metadata_mode=MetadataMode.EMBED)
-#     )
-#     node.embedding = node_embedding
+for node in tqdm.tqdm(nodes):
+    node_embedding = embed_model.get_text_embedding(
+        node.get_content(metadata_mode=MetadataMode.EMBED)
+    )
+    node.embedding = node_embedding
 
-# vector_store.add(nodes)
+vector_store.add(nodes)
 
 # ----- QUERY DB ----- #
 
@@ -83,14 +96,26 @@ def instantiate_db():
     db = chromadb.PersistentClient(path="./chroma_db")
     return db
 
-def recommend_products(query: str, db = None):
+def recommend_products(query: str, db = None, min_rating: float = 0, min_reviews: int = 100, max_price: float = 10):
     if db is None:
         db = instantiate_db()
     giftcards_collection = db.get_or_create_collection("giftcards")
     vector_store = ChromaVectorStore(chroma_collection=giftcards_collection)
-    template = "You are a product recommendation expert, tasked with recommending products to users based on what they say they want, and what could be good for them. Based on the provided dataset of products and reviews, recommend the top 3 products for a given query. Return only the ASIN of the recommended products, separated by a comma. The user query is as follows {query}.".format(query=query)
+
+    # Build hardcoded filters
+    filters = MetadataFilters(
+        filters=[
+            MetadataFilter(key="average_rating", value=min_rating, operator=FilterOperator.GTE),
+            MetadataFilter(key="rating_number", value=min_reviews, operator=FilterOperator.GTE),
+            MetadataFilter(key="price", value=max_price, operator=FilterOperator.LTE),
+            ]
+        )
+
+    # Set up LLM filter
+    template = "You are a product recommendation expert, tasked with recommending products to users based on what they say they want, and what could be good for them. Based on the provided dataset of products, reviews and the users query, recommend the top 3 products for a given query. Return only the ASIN of the recommended products, separated by a comma. If there is no good match, return an empty list. Overindex on the properties that the user has stated, and think deeply about how every word the user have given in the query could impact what they're looking for, assuming they have chosen each word deliberately. The user query is as follows {query}.".format(query=query)
     index = VectorStoreIndex.from_vector_store(vector_store)
-    query_engine = index.as_query_engine()
+    query_engine = index.as_query_engine(filters=filters)
+    
     response = query_engine.query(template)
     return response.response
 
@@ -104,11 +129,11 @@ def get_product_details(asin: str, db = None):
 
 def justify_recommendation(query: str, product: dict):
     # Use the LLM to justify the recommendation with a list of pros, cons and neutral features
-    base_template = "You are a product recommendation expert, tasked with recommending products to users based on what they say they want, and what could be good for them. The user has provided the following query: {query}. You have recommended the following product: {product_data}.".format(query=query, product_data=product)
-    summary_template = base_template + " Based on the user query and all the information about the product, provide a newline-separated list of the reasons why you recommended this product, focusing on how it matches the user query. Structure your response as follows: 'I recommended this product for this query because: [REASONS].'"
-    positive_template = base_template + " Based on the user query and all the information about the product, provide a newline-separated list of the positive features of the product. Remove any call to action e.g., 'see below' or 'learn more'."
-    negative_template = base_template + " Based on the user query and all the information about the product, provide a newline-separated list of the negative features of the product. Remove any call to action e.g., 'see below' or 'learn more'."
-    neutral_template = base_template + " Based on the user query and all the information about the product, provide a newline-separated list of the neutral features of the product. Remove any call to action e.g., 'see below' or 'learn more'."
+    base_template = "You are a product recommendation expert, tasked with recommending products to users based on what they say they want, and what could be good for them. Be as concise as possible, limiting to around 30 characters. Do not include any call to action e.g., 'see below' or 'learn more'. I have provided the following query: {query}. You have recommended the following product: {product_data}.".format(query=query, product_data=product)
+    summary_template = base_template + " Based on my query and all the information about the product, provide a newline-separated list of the reasons why you recommended this product, focusing on how it matches my query."
+    positive_template = base_template + " Based on my query and all the information about the product, provide a newline-separated list of the positive features of the product, prioritising those that best serve my original query. Thinky deeply about why these features are important to me and whether they are truly beneficial based on the query."
+    negative_template = base_template + " Based on my query and all the information about the product, provide a newline-separated list of the negative features of the product which may prevent me from purchasing this product. Think deeply about why these features could impact me based on the query."
+    neutral_template = base_template + " Based on my query and all the information about the product, provide a newline-separated list of the neutral features of the product. These features are useful to know but may not necessarily be a deciding factor my purchasing decision. Think deeply about why these features could impact me based on the query."
     
     summary_response = OpenAI().complete(summary_template).text
     positive_response = OpenAI().complete(positive_template).text
@@ -117,18 +142,14 @@ def justify_recommendation(query: str, product: dict):
 
     return summary_response, positive_response, negative_response, neutral_response
 
-"""
-STEPS:
-1. You are a product recommendation expert, tasked with recommending products to users based on what they say they want, and what could be good for them. Based on the provided dataset of products and reviews, recommend the top 3 products for a given query. Return only the ASIN of the recommended products, separated by a comma. The user query is as follows {}.
-2. You are a product recommendation expert, tasked with recommending products to users based on what they say they want, and what could be good for them. The user has provided the following query: {}. You have recommended the following product: {}. Based on the user query and all the information about the product, provided a bullet-point list of the reasons why you recommended this product. Structure your response as follows: "I recommended this product for this query because: {reasons}. The product has the following additional positive features: {features}. The product has the following additional negative features: {features}. The product has the following additional neutral features: {features}."
-3. Repeat step 2 for the top 3 recommended products.
-"""
-
 if __name__ == "__main__":
-    query = "gift card for a friend"
+    query = "a sick pair of trousers for my emo friend"
     response = recommend_products(query).split(",")
     print(response)
     details = get_product_details(response[0])
-    print(details)
-    justification = justify_recommendation(query, details[0])
-    print(justification)
+    if details:
+        print(details)
+        justification = justify_recommendation(query, details[0])
+        print(justification)
+    else:
+        print("No product match")        
--- ui.py
+++ ui.py
@@ -1,77 +1,102 @@
 """
 UI
 """
-from nicegui import ui, run
+from nicegui import ui, run, app
 from llm import recommend_products, get_product_details, instantiate_db, justify_recommendation
 
-# Instantiatiate ChromaDB on startup, asynchronously
-db = instantiate_db()
+db = None
+
+def on_startup():
+    # Instantiatiate ChromaDB on startup, asynchronously
+    global db
+    db = instantiate_db()
 
 AMAZON_PRODUCT_URL = "https://www.amazon.com/exec/obidos/ASIN/{ASIN}"
 
-@ui.page("/")
+app.on_startup(on_startup)
+
+@ui.page("/", title="Home")
 def main():
 
-    # This page sets up the search bar. User adds query and 'hardcoded' settings, then clicks search and is redirected to the results page
-    with ui.button_group():
-        query = ui.input(placeholder="What do you want from a giftcard?").classes("w-96")
-        ui.button(icon="search", on_click=lambda: ui.navigate.to(f"/results?q={query.value}") )
+    # This page sets up the search bar. 
+    # User adds query and 'hardcoded' settings, then clicks search and is redirected to the results page
+    with ui.button_group().classes("mt-64 mx-auto items-center justify-center"):
+        query = ui.input(placeholder="What do you want from a giftcard?").classes("w-96 px-2")
+        ui.button(icon="search", on_click=lambda: ui.navigate.to(f"/results?q={query.value}&min_rating={min_rating.value}&min_reviews={min_reviews.value}&max_price={max_price.value}") )
 
-    ui.separator()
+    ui.separator().classes("w-1/2 items-center justify-center mx-auto")
 
     # Add hardcoded settings here
-    ui.label("Settings:")
-    min_rating = ui.number(label="Minimum rating", min=0, max=5, step=0.5, value=0)
-    ui.label("Price range")
-    price_range = ui.range(min=0, max=100, step=1, value=[0, 100])
+    ui.markdown("#### Settings:").classes("mx-auto")
+    with ui.grid(columns=3).classes("items-center justify-center mx-auto h-full"):
+        min_rating = ui.number(label="Min. rating", min=0, max=5, step=0.5, value=4, suffix="★")
+        min_reviews = ui.number(label="Min. # reviews", min=0, max=None, step=1, value=100)
+        max_price = ui.number(label="Max price", min=1, max=1000, step=1, value=50, prefix="$")
 
-@ui.page("/results")
-def results(q: str):
+@ui.page("/results", response_timeout=60)
+async def results(q: str, min_rating: float = 0, min_reviews: int = 100, max_price: int = 50):
+
+    def build_results(q, min_rating, min_reviews, max_price):
+        results_list = []
+        response = recommend_products(q, db=db, min_rating=min_rating, min_reviews=min_reviews, max_price=max_price).split(",")
+        response = [x.strip() for x in response]
+        for asin in response:
+            product_data = get_product_details(asin, db=db)
+            summary, positives, negatives, neutrals = justify_recommendation(q, product_data[0])
+            summary_list = summary.split("\n-")
+            positives_list = positives.split("\n-")
+            negatives_list = negatives.split("\n-")
+            neutrals_list = neutrals.split("\n-")
+            results_list.append({
+                "asin": asin,
+                "product_data": product_data,
+                "summary": summary_list,
+                "positives": positives_list,
+                "negatives": negatives_list,
+                "neutrals": neutrals_list
+            })
+        return results_list
 
     # Show loading spinner while awaiting results.
-    results_dict = {}
-    loading = ui.spinner()
-    response = recommend_products(q, db=db).split(",")
-    response = [x.strip() for x in response]
-    top_asin = response[0]
-    for asin in response:
-        product_data = get_product_details(asin, db=db)
-        summary, positives, negatives, neutrals = justify_recommendation(q, product_data[0])
-        summary_list = summary.split("\n-")
-        positives_list = positives.split("\n-")
-        negatives_list = negatives.split("\n-")
-        neutrals_list = neutrals.split("\n-")
-        results_dict[asin] = {
-            "product_data": product_data,
-            "summary": summary_list,
-            "positives": positives_list,
-            "negatives": negatives_list,
-            "neutrals": neutrals_list
-        }
-    loading.delete()
+    with ui.row() as loading_row:
+        ui.spinner(type="bars", size="xl")
+        ui.label("Loading results...")
+    results_list = await run.io_bound(build_results, q, min_rating, min_reviews, max_price)
+    loading_row.delete()
 
     # Display results
-    ui.label("Top result")
-    with ui.card():
-        top_result = results_dict[top_asin]
-        ui.label(top_result["product_data"][0]["title"])
-        with ui.row():
-            ui.image(top_result["product_data"][0]["images"]).classes("w-48")
-            with ui.column():
-                ui.label("Rating: " + str(top_result["product_data"][0]["average_rating"]) + " (" + str(top_result["product_data"][0]["rating_number"]) + " reviews)")
-                ui.label("Price: " + "$" + str(top_result["product_data"][0]["price"]))
-                for line in top_result["summary"][1:4]:
+    ui.label("We think you'll like...").classes("text-2xl underline")
+    with ui.carousel(arrows=True, navigation=True, animated=True).classes("w-full h-full").props("control-color=black control-type=unelevated"):
+        for result in results_list:
+            with ui.carousel_slide().classes("items-center justify-center w-full h-full"):
+                results_card(result)
+
+
+def results_card(result_data):
+    with ui.card().classes("w-3/4 mb-12"):
+        with ui.row().classes("w-full"):
+            ui.label(result_data["product_data"][0]["title"]).classes("text-xl")
+            ui.space()
+            with ui.link("", target=AMAZON_PRODUCT_URL.format(ASIN=result_data["product_data"][0]["parent_asin"]), new_tab=True):
+                ui.image("https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg").classes("w-24")
+                ui.tooltip("View on Amazon")
+        with ui.row().classes("w-full"):
+            ui.image(result_data["product_data"][0]["images"]).classes("w-48")
+            with ui.column().classes("justify-right items-right"):
+                ui.markdown("## **Price:** " + "$" + str(result_data["product_data"][0]["price"]))
+                ui.markdown("**Rating:** " + str(result_data["product_data"][0]["average_rating"]) + " *(" + str(result_data["product_data"][0]["rating_number"]) + " reviews)*")
+                for line in result_data["summary"][0:3]:
                     ui.chip(text=line, icon="info")
-                ui.link("View on Amazon", target=AMAZON_PRODUCT_URL.format(ASIN=top_result["product_data"][0]["parent_asin"]), new_tab=True)
-        with ui.row():
+        ui.markdown("**Review summaries**")
+        with ui.grid(columns=3):
             with ui.column():
-                for line in top_result["positives"][1:4]:
+                for line in result_data["positives"][0:3]:
                     ui.chip(text=line, color="green")
             with ui.column():
-                for line in top_result["negatives"][1:4]:
+                for line in result_data["negatives"][0:3]:
                     ui.chip(text=line, color="red")
             with ui.column():
-                for line in top_result["neutrals"][1:4]:
+                for line in result_data["neutrals"][0:3]:
                     ui.chip(text=line, color="gray")
 
 if __name__ in {"__main__", "__mp_main__"}:
